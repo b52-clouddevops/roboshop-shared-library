97d74d2199ad8dccb0d4d6274d42f194e174a2a9
CodingManoj <mvp2612@hotmail.com>
2023-02-02 19:46:28 -0500

    AutoPush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,7 +9,8 @@ def lintChecks(COMPONENT) {
 def sonarChecks(COMPONENT) {
         sh "echo Starting Code Quality Analysis"
         sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000  -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT}  -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
-        sh "sonar-quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
+        sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gata.sh"
+        sh "./sonar-quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
 }
 
 def call(COMPONENT)                                              // call is the default function that's called by default.
