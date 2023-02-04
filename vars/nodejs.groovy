c4262ac3eab0d2d6d876d6d8599ec58dd8a2199d
CodingManoj <mvp2612@hotmail.com>
2023-02-03 20:47:02 -0500

    AutoPush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -60,7 +60,7 @@ def call(COMPONENT)                                              // call is the 
                 steps {
                     sh "checking whether artifact exists of not. If it doesn't exist, then only proceed with Preparation and Upload"
                     script {
-                        env.STATUS_CODE=sh(returnStdout: true, script: 'curl http://172.31.4.26:8081/service/rest/${COMPONENT}/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}')
+                        env.STATUS_CODE=sh(returnStdout: true, script: 'curl http://172.31.4.26:8081/service/rest/${COMPONENT}/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip')
                         print STATUS_CODE
                     }
                 }
