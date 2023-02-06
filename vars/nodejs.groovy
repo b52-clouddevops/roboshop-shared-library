162b19f7bcb8d61d85d63aa68c51ced55217845d
CodingManoj <mvp2612@hotmail.com>
2023-02-05 19:38:21 -0500

    Adding all the know and well recognized resources

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -60,8 +60,8 @@ def call(COMPONENT)                                              // call is the 
                 steps {
                     sh "echo checking whether artifact exists of not. If it doesnt exist then only proceed with Preparation and Upload"
                     script {
-    env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip" )
-    print UPLOAD_STATUS
+                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
+                        print UPLOAD_STATUS
                     }
                 }
             }
