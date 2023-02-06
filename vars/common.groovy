1a199988b7aadf295e09b8cc72e336a91d517199
CodingManoj <mvp2612@hotmail.com>
2023-02-05 20:57:03 -0500

    AutoPush

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,8 +1,63 @@
-def sonarChecks(COMPONENT) {   
-       sh  "echo Starting Code Quality Analysis"
-       // sh  "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT}  -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
-       // sh  "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
-       // sh  "bash -x quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
-       // Uncomment above 3 lines only when your sonarQube is available. Keeping cost in mind, we marked them as comments. But, in lab we have practiced the sonar checks and quality gates.
-       sh "echo Code Quality Checks Completed."
+def sonarChecks() {   
+       stage('Sonar Checks') 
+              {
+                     sh  "echo Starting Code Quality Analysis"
+                     // sh  "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000  ${ARGS} -Dsonar.projectKey=${COMPONENT}  -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+                     // sh  "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
+                     // sh  "bash -x quality-gata.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
+                     // Uncomment above 3 lines only when your sonarQube is available. Keeping cost in mind, we marked them as comments. But, in lab we have practiced the sonar checks and quality gates.
+                     sh "echo Code Quality Checks Completed."
+              }
 }
+
+
+def lintChecks() {
+       stage('Lint Checks') {
+          if(env.APP == "maven") {      
+                     sh "echo Installing mvn"
+                     // sh "yum install maven -y"
+                     // sh "mvn checkstyle:check"
+                     sh "echo lint checks completed for ${COMPONENT}.....!!!!!"             
+          }
+          else if(env.APP == "nodejs") {
+                     sh "echo Installing JSLINT"
+                     sh "npm install jslint"
+                     sh "ls -ltr node_modules/jslint/bin/"
+                     // sh "./node_modules/jslint/bin/jslint.js server.js"
+                     sh "echo lint checks completed for ${COMPONENT}.....!!!!!"              
+          }
+          else if(env.APP == "python") {
+                     sh "echo Installing PYLINT"
+                     // https://pylint.pycqa.org/en/2.7.1/user_guide/run.html 
+                     // sh pylist filename.py
+                     sh "echo lint checks completed for ${COMPONENT}.....!!!!!"        
+          }
+          else if(env.APP = "angularjs") {
+                      sh "echo Installing ANGULAR LINT"
+                      sh "echo lint checks completed for ${COMPONENT}.....!!!!!"
+          }
+          else 
+                     sh "echo performing generic lint cheks"
+    }
+}
+
+
+def testCases() {
+       parallel(                                                        // This is how we write stages in parallel.
+                "UNIT": {
+                     stage('Unit Tests'){
+                              sh "echo Unit Testing ......."                            
+                        }
+                    },
+                "INTEGRATION": {
+                     stage('Integration Tests'){
+                              sh "echo Integration Testing ......."                          
+                        }
+                    },
+                "FUNCTIONAL": {
+                     stage('Functional Tests'){
+                              sh "echo Functional Testing ......."                          
+                        }
+                    },
+            )
+       }
--- vars/python.groovy
+++ vars/python.groovy
@@ -1,96 +1,98 @@
-def lintChecks(COMPONENT) {
-        sh "echo Installing PYLINT"
-        // https://pylint.pycqa.org/en/2.7.1/user_guide/run.html 
-        // sh pylist filename.py
-        sh "echo lint checks completed for ${COMPONENT}.....!!!!!"
-}
-
-def call(COMPONENT)                                              // call is the default function that's called by default.
-{
-    pipeline {
-        agent any 
-        environment {
-            SONAR = credentials('SONAR')
-            NEXUS = credentials('NEXUS')
-            SONAR_URL = "172.31.1.207"
-            NEXUS_URL = "172.31.4.26"
-        }
-        stages {                                        // Start of Stages
-            stage('Lint Checks') {
-                steps {
-                    script {
-                        lintChecks(COMPONENT)                    // If the function is in the same file, no need to call the function with the fileName as prefix.
-                    }
-                }
-            }
-
-            stage('Sonar Checks') {
-                steps {
-                    script {
-                        env.ARGS="-Dsonar.sources=."
-                        common.sonarChecks(COMPONENT)                    
-                    }
-                }
-            }
-
-            stage('Test Cases') {
-                    parallel {
-                        stage('Unit Tests'){
-                            steps {
-                                sh "echo Unit Testing ......."
-                            }
-                        }
-
-                        stage('Integration Tests'){
-                            steps {
-                                sh "echo Integration Testing ......."
-                            }
-                        }
-
-                        stage('Functional Tests'){
-                            steps {
-                                sh "echo Functional Testing ......."
-                            }
-                        }
-                    }
-            }
-
-            stage('Artifact Validation On Nexus') {
-                when { 
-                    expression { env.TAG_NAME != null } 
-                    }
-                steps {
-                    sh "echo checking whether artifact exists of not. If it doesnt exist then only proceed with Preparation and Upload"
-                    script {
-                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
-                        print UPLOAD_STATUS
-                    }
-                }
-            }
-
-            stage('Preparing the artifact') {
-                when { 
-                    expression { env.TAG_NAME != null } 
-                    expression { env.UPLOAD_STATUS == "" }
-                    }
-                steps {
-
-                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini requirements.txt"
-                    sh "ls -ltr"
-                }
-            }
-            stage('Uploading the artifact') {
-                when { 
-                    expression { env.TAG_NAME != null } 
-                    expression { env.UPLOAD_STATUS == "" }
-                    }
-                steps {
-                    sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
-                }
-            }
-        } // End of Stages
+def call() {
+    node {
+        env.APP = "python"
+        lintChecks()
+        env.ARGS="-Dsonar.sources=."
+        common.sonarChecks()  
+        common.testCases() 
     }
 }
+// def call(COMPONENT)                                              // call is the default function that's called by default.
+// {
+//     pipeline {
+//         agent any 
+//         environment {
+//             SONAR = credentials('SONAR')
+//             NEXUS = credentials('NEXUS')
+//             SONAR_URL = "172.31.1.207"
+//             NEXUS_URL = "172.31.4.26"
+//         }
+//         stages {                                        // Start of Stages
+//             stage('Lint Checks') {
+//                 steps {
+//                     script {
+//                         lintChecks(COMPONENT)                    // If the function is in the same file, no need to call the function with the fileName as prefix.
+//                     }
+//                 }
+//             }
+
+//             stage('Sonar Checks') {
+//                 steps {
+//                     script {
+//                         env.ARGS="-Dsonar.sources=."
+//                         common.sonarChecks(COMPONENT)                    
+//                     }
+//                 }
+//             }
+
+//             stage('Test Cases') {
+//                     parallel {
+//                         stage('Unit Tests'){
+//                             steps {
+//                                 sh "echo Unit Testing ......."
+//                             }
+//                         }
+
+//                         stage('Integration Tests'){
+//                             steps {
+//                                 sh "echo Integration Testing ......."
+//                             }
+//                         }
+
+//                         stage('Functional Tests'){
+//                             steps {
+//                                 sh "echo Functional Testing ......."
+//                             }
+//                         }
+//                     }
+//             }
+
+//             stage('Artifact Validation On Nexus') {
+//                 when { 
+//                     expression { env.TAG_NAME != null } 
+//                     }
+//                 steps {
+//                     sh "echo checking whether artifact exists of not. If it doesnt exist then only proceed with Preparation and Upload"
+//                     script {
+//                         env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true" )
+//                         print UPLOAD_STATUS
+//                     }
+//                 }
+//             }
+
+//             stage('Preparing the artifact') {
+//                 when { 
+//                     expression { env.TAG_NAME != null } 
+//                     expression { env.UPLOAD_STATUS == "" }
+//                     }
+//                 steps {
+
+//                     sh "zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini requirements.txt"
+//                     sh "ls -ltr"
+//                 }
+//             }
+//             stage('Uploading the artifact') {
+//                 when { 
+//                     expression { env.TAG_NAME != null } 
+//                     expression { env.UPLOAD_STATUS == "" }
+//                     }
+//                 steps {
+//                     sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+//                 }
+//             }
+//         } // End of Stages
+//     }
+// }
 
 
 // Jenkins WS Should be operated with t3.medium for the jobs to be processed quickly.
