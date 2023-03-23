2a0172ad3d4e728e7f16f8c2e5729cefff35e97f
CodingManoj <mvp2612@hotmail.com>
2023-03-23 19:40:17 -0400

    AutoPush

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,7 +4,6 @@ def call() {
     node {
         sh "rm -rf *"
         git branch: 'main', url: "https://github.com/b52-clouddevops/${COMPONENT}.git"
-        env.APP = "angularjs"
         common.lintChecks()
 
 
@@ -12,13 +11,44 @@ def call() {
                 sh "docker build -t 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest ."
                 sh "docker images "
         }
-    if ( env.TAG_NAME != null ) {
-        stage('Docker Push') {
-            sh "docker tag 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
-            sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 355449129696.dkr.ecr.us-east-1.amazonaws.com"
-            sh "docker push 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
-            }
 
+        if ( env.TAG_NAME != null ) {
+            stage('Docker Push') {
+
+                if (env.APP == "nodejs") {
+                sh '''
+                    npm install 
+                    zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js schema
+                '''
+                }
+
+                if (env.APP == "java") {
+                sh '''
+                    mvn package 
+                    cp target/${COMPONENT}-1.0.jar ${COMPONENT}.jar 
+                    zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar schema
+                '''
+                }
+
+                if (env.APP == "python") {
+                sh '''
+                    zip -r ${COMPONENT}-${TAG_NAME}.zip *.ini *.py *.txt
+                '''
+                }
+
+                if (env.APP == "nginx") {
+                sh '''
+                    zip -r ${COMPONENT}-${TAG_NAME}.zip *
+                    zip -d ${COMPONENT}-${TAG_NAME}.zip Jenkinsfile 
+                '''
+                }
+
+                sh "docker tag 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
+                sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 355449129696.dkr.ecr.us-east-1.amazonaws.com"
+                sh "docker push 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
+
+                }
+            }
         }
     }
-}
+
